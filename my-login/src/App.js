844f58023926f160b0ed2da5aa5ad50d93fd79e3
Luiz Anthonio <anthonioluiz5@gmail.com>
2023-01-10 00:05:57 -0300

    rotas

---
+++ my-login/src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+
+import AppRoutes from './AppRoutes';
+
+//import LoginPage from './pages/LoginPage';
+
+//import HomePage from './pages/HomePage';
+
+
+function App() {
+  return (
+    <div className="App">
+     
+
+      <AppRoutes/>
+
+    
+
+    </div>
+  );
+}
+
+export default App;
